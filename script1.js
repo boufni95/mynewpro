4068ea1c7f572bdd1a3bd282389d73473b073b2a
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-05-06 11:11:04 +0200

    choof had chi a khay hatim

--- script1.js
+++ script1.js
@@ -1,1 +1,15 @@
-var menuItem=document.getElementById("menu");
+var menuItem=document.getElementById("menu").getElementsByTagName("li")
+var btn=document.getElementById("bt")
+var headline=document.getElementById("demo");
+menuItem[0].innerHTML = "hello world"
+for(i=0;i<3;i++){
+    menuItem[i].addEventListener("click",selectItem);
+}
+function selectItem(){
+   headline.innerHTML=this.innerHTML;
+}
+btn.addEventListener("click",newItem)
+function newItem(){
+    headLine.innerHTML="hello world";
+    btn.innerHTML="safi baraka"
+}
